cbc6d24b68d9b1b1c103d02631e4f53c1965f803
Takeru Kakuta <kakuta19960915@gmail.com>
2025-09-04 18:28:53 +0900

    feat: TodoListコンポーネントの作成

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { InputForm } from "./components/InputForm";
 import { Title } from "./components/Title";
+import { TodoList } from "./components/TodoList";
 
 function App() {
   return (
     <div className="body">
       <Title />
       <InputForm />
+      <TodoList />
     </div>
   );
 }
