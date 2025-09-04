ae27488b23997fc74f7f2f1a015a0a6fa8f06727
Takeru Kakuta <kakuta19960915@gmail.com>
2025-09-04 18:22:06 +0900

    feat: InputFormコンポーネントの作成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import { InputForm } from "./components/InputForm";
 import { Title } from "./components/Title";
 
 function App() {
   return (
     <div className="body">
       <Title />
+      <InputForm />
     </div>
   );
 }
